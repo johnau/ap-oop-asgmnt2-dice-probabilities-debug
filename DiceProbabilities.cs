6ea93c4b96fd146a8bacbfee4af81637e575f9da
John McSweeney <johnmcsweeney88@gmail.com>
2024-02-16 19:45:13 +0800

    console out + comments

--- DiceProbabilities.cs
+++ DiceProbabilities.cs
@@ -2,80 +2,87 @@
 
 namespace DiceProbabilitiesDebug;
 
-class DiceProbabilities
+public static class DiceProbabilities
 {
     public static Dictionary<int, Double> CalculateProbabilitiesForNumberOfDice(int n)
     {
         Dictionary<int, int> rollCalculations = new Dictionary<int, int>(); 
         // int mn = n; // replace mn in code with n
         int mx = n * 6;
-        Debug.WriteLine($"Populate RollCalc dictionary for {n} dice (from {n} to {mx})");
+        Console.WriteLine($"Populate RollCalc dictionary for {n} dice (from {n} to {mx}) with 0's");
         for (int i = n; i <= mx; i++) // populate dict with keys from number of dice to number of dice times the sides (6)
         {
             rollCalculations[i] = 0;
-            Debug.WriteLine($"{i}: {0}");
+            Console.WriteLine($"{i}: 0");
         }
         
         int[] dice = new int[n]; // new array sized at number of dice.
-        Debug.WriteLine($"Populate array of ({n}) ({n} dice)");
+        Console.WriteLine($"Populate array of ({n}) ({n} dice)");
         for (int i = 0; i < n; i++)
         {
             dice[i] = 1;
-            Debug.WriteLine($"{i}: {1}");
+            Console.WriteLine($"{i}: {1}");
         }
 
         bool finished1 = false;
-        Debug.WriteLine("Begin main outer loop");
+        Console.WriteLine("Begin main outer loop");
         while (!finished1)
         {
-            Debug.WriteLine($"Next outer");
+            Console.WriteLine($"Next outer");
             //int total = 0;
             //foreach (int d in dice)
             //{
             //    total += d;
             //}
             int total = dice.Sum();
-            Debug.WriteLine($"Summation of 'dice' array={total}");
+            Console.WriteLine($"Summation of 'dice' array={total}");
 
             rollCalculations[total] += 1;
-            Debug.WriteLine($"Mutate RollCalcs dict, update key{total}={rollCalculations[total]} (from {rollCalculations[total]-1})");
+            Console.WriteLine($"Mutate RollCalcs dict, update key{total}={rollCalculations[total]} (from {rollCalculations[total]-1})");
 
             int i = 0;
             bool finished2 = false;
-            Debug.WriteLine("Begin main inner loop");
+            Console.WriteLine("Begin main inner loop");
             while (!finished2)
             {
-                Debug.WriteLine($"Next inner ({i})");
+                Console.WriteLine($"Next inner ({i})");
                 dice[i] += 1;
-                Debug.WriteLine($"Update dice array [{i}]={dice[i]}");
+                Console.WriteLine($"Update dice array [{i}]={dice[i]}");
                 if (dice[i] <= 6)
                 {
-                    Debug.WriteLine($"dice array [{i}] value is less than 7 ({dice[i]}), main inner loop cancelled.");
+                    Console.WriteLine($"dice array [{i}] value is less than 7 ({dice[i]}), main inner loop cancelled.");
                     finished2 = true;
                 }
                 else
                 {
-                    Debug.WriteLine($"dice array [{i}] value is now more than 6 ({dice[i]}), main inner loop active.");
+                    Console.WriteLine($"dice array [{i}] value is now more than 6 ({dice[i]}), main inner loop active.");
                     if (i == n - 1)
                     {
-
+                        Console.WriteLine($"Main out and inner loops exit (i({i}) == n({n}) - 1");
                         finished1 = true;
                         finished2 = true;
                     }
                     else
                     {
                         dice[i] = 1;
+                        Console.WriteLine($"Update dice array[{i}]=1");
                     }
                 }
                 i++;
             }
         }
-        Dictionary<int, Double> rp = new Dictionary<int, double>();
+
+        Dictionary<int, Double> rollProbabilities = new Dictionary<int, double>();
         Double total2 = Math.Pow(6.0, (Double)n);
+        Console.WriteLine($"Total combinations: 6^{n}={total2}");
+        Console.WriteLine($"Process RollCalc dictionary for {n} dice (from {n} to {mx}) into roll probablities dict");
         for (int i = n; i <= mx; i++)
         {
-            rp[i] = (Double)rollCalculations[i] / total2;
+            Console.WriteLine($"Roll calc [{i}]={rollCalculations[i]}");
+            Console.WriteLine($"Roll divided by Total combinations = {(Double)rollCalculations[i] / total2}");
+            rollProbabilities[i] = (Double)rollCalculations[i] / total2;
+            Console.WriteLine($"Roll probability for [{i}] = {(Double)rollCalculations[i] / total2}");
         }
-        return rp;
+        return rollProbabilities;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DiceProbabilitiesDebug;
+using System.Diagnostics;
+
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("\n \n Calculate probabilities for 1 die \n \n ");
+DiceProbabilities.CalculateProbabilitiesForNumberOfDice(1);
+Console.WriteLine("\n \n Calculate probabilities for 2 dice \n \n ");
+DiceProbabilities.CalculateProbabilitiesForNumberOfDice(2);
